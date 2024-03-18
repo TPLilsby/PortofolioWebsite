15fbcd00892b19e258404cee1c813a7d85e24e4a
Tobias Lilsby <ttlilsby@gmail.com>
2024-03-18 17:36:00 +0100

    Addons

--- PortofolioWebsite/script.js
+++ PortofolioWebsite/script.js
@@ -15,4 +15,31 @@ $(document).ready(function() {
       $('#aboutMeText').slideDown();
     });
   });
+
   
+  // valider formular
+  const form = document.getElementById('contactForm');
+    const emailInput = document.getElementById('email');
+    const phoneInput = document.getElementById('phone');
+    
+    form.addEventListener('submit', function(event) {
+      if (!validateEmail(emailInput.value)) {
+        event.preventDefault();
+        alert('Indtast venligst en gyldig e-mailadresse.');
+      }
+
+      if (!validatePhone(phoneInput.value)) {
+        event.preventDefault();
+        alert('Telefonnummeret må kun indeholde numeriske tegn.');
+      }
+    });
+
+    function validateEmail(email) {
+      const re = /\S+@\S+\.\S+/;
+      return re.test(email);
+    }
+
+    function validatePhone(phone) {
+      const re = /^\d+$/;
+      return re.test(phone);
+    }
